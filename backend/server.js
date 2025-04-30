60fd44c2a57505df1316edb7c9938b5e29cc3dbb
SarahKim1006 <109427938+SarahKim1006@users.noreply.github.com>
2025-04-29 20:37:23 -0400

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
-const express = require('express');
-const cors = require('cors');
-const { Configuration, OpenAIApi } = require('openai');
-require('dotenv').config();
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { OpenAI } from 'openai';
 
+dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
